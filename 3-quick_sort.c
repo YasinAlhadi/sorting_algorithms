83341b02e5bfe8321b9744b56dadb0533030c549
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-11-12 18:10:48 +0000

    Quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -14,22 +14,28 @@ int partition(int *array, size_t size, int lo, int hi)
 	int pivot_v, j, i, tmp;
 
 	pivot_v = array[hi];
-	j = 0;
 	i = lo - 1;
-	for (j = lo; j <= hi - 1; j++)
+	for (j = lo; j <= hi; j++)
 	{
-		if (array[j] <= pivot_v)
+		if (array[j] < pivot_v)
 		{
 			i++;
-			tmp = array[j];
-			array[j] = array[i];
-			array[i] = tmp;
-			print_array(array, size);
+			if (i != j)
+			{
+				tmp = array[i];
+				array[i] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
 		}
 	}
-	tmp = array[hi];
-	array[hi] = array[i + 1];
-	array[i + 1] = tmp;
+	if (array[hi] < array[i + 1])
+	{
+		tmp = array[i + 1];
+		array[i + 1] = array[hi];
+		array[hi] = tmp;
+		print_array(array, size);
+	}
 	return (i + 1);
 }
 
@@ -63,7 +69,5 @@ void quicksort_recs(int *array, size_t size, int lo, int hi)
  */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL)
-		return;
 	quicksort_recs(array, size, 0, size - 1);
 }
